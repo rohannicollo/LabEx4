35b8298b3e5643f03451760536e78fdd073e2477
Rohan Teope <rohannicollo@gmail.com>
2026-05-14 14:45:05 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/labex4/LabEx4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labex4;
+
+/**
+ *
+ * @author User
+ */
+public class LabEx4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
