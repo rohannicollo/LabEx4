1985c3721972091e54d21866e6d007b651df54f4
Rohan Teope <rohannicollo@gmail.com>
2026-05-15 11:03:58 +0800

    part 3

--- src/main/java/BT.java
+++ src/main/java/BT.java
@@ -1,82 +1,8 @@
-<<<<<<< Updated upstream
 // Rohan Nicollo M. Teope
 // Sean Gabriel DR. Bolalin
 // Course: ICS 2605
 // Section: 1CSD
 // Lab Exercise 4
-
-import java.util.*;
-
-public class BT<T> {
-    
-    public BTNode<T> root;
-    public int height;
-    
-    public BT() {
-        root = null;
-        height = -1;
-    }
-    
-    public void setRoot(BTNode<T> n) {
-        root = n;
-        height = 0;
-        root.level = 0;
-    }
-    
-    public void computeLevels() {
-        
-        if (root != null) {
-            
-            SLL<BTNode<T>> que = new SLL<>();
-            int level = 0;
-            que.addToTail(root);
-            
-            while (!que.isEmpty()) {
-                
-                int size = que.size();
-                
-                for (int i = 0; i < size; i++) {
-                    
-                    BTNode<T> n = que.deleteFromHead();
-                    n.level = level;
-                    System.out.println("K=" + n.info + " level=" + n.level);
-                    
-                    if (n.left != null) {
-                        que.addToTail(n.left);
-                    }
-                    
-                    if (n.right != null) {
-                        que.addToTail(n.right);
-                    }
-                    
-                }
-                
-                level++;
-                
-            }
-            
-            height = level - 1;
-            
-        }
-    }
-    
-    public String toString() {
-        
-        if (root == null) {
-            return "ht=undefined (tree is empty.)";
-        }
-        
-        return "ht=" + height + " " + root.toString();
-        
-    }
-}
-=======
-// Rohan Nicollo M. Teope, Sean Gabriel DR. Bolalin
-// Course: ICS 2605
-// Section: 1CSD
-// Lab Exercise 4
-
-import java.util.*;
 
 public class BT<T extends Comparable<? super T>> {
     
@@ -162,4 +88,3 @@ public class BT<T extends Comparable<? super T>> {
         
     }
 }
->>>>>>> Stashed changes
